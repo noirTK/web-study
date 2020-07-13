8745129c554739875355a9b10a5bc43c534feb38
noir8TK <plaire@hanyang.ac.kr>
2020-07-13 18:48:40 +0900

    update meta testpage

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,19 @@
-
 const list = document.createElement('ul');
 const info = document.createElement('p');
-info.textContent = "동적 리스트. 페이지를 클릭해서 리스트 추가하기. 추가된 리스트 아이템을 클릭해 아이템 변경."
 
+info.textContent = "화면을 클릭해 리스트에 아이템을 입력하세요. 리스트 안의 아이템을 클릭하여 아이템을 수정할 수도 있습니다."
 document.body.appendChild(info);
 document.body.appendChild(list);
 
-document.querySelector('html').onclick = function()
+document.querySelector('html'),onclick = function()
 {
     const item = document.createElement('li');
-    item.textContent = prompt('추가할 내용을 입력해주세요.');
+    item.textContent = prompt("리스트에 추가하기를 희망하는 아이템을 입력하세요.");
     list.appendChild(item);
-
+    
     item.onclick = function(event)
     {
         event.stopPropagation();
-        this.textContent = prompt('수정할 내용을 입력해주세요.');
+        this.textContent = prompt("변경하기를 희망하는 아이템을 입력해주세요.");
     }
 }
